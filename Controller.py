8d29268058255e764ed22108286660045baec7c9
Rohan <rohan.prasad2015@vit.ac.in>
2017-01-07 22:21:06 +0530

    Fix a bug

--- Controller.py
+++ Controller.py
@@ -1,5 +1,12 @@
 import subprocess
 from subprocess import Popen
-command1 = 'python3 /home/rohan/GDG/Vehicle_Mark5.py "/home/rohan/GDG/video2.avi"'
+command1 = 'python3 /home/rohan/traffic-light/Vehicle.py "/home/rohan/GDG/video2.avi"'
 
 p = Popen(command1, shell = True, stdout = subprocess.PIPE)
+
+while True:
+    ret = p.returncode()
+
+    print (ret)
+    if ret < 0 or ret == None:
+        break
